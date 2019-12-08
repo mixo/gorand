c2c0c6f4b4f7715c021b310a1ae48aab1cd13473
Alexey Ohorzin <ohor.info@gmail.com>
2019-12-08 22:07:43 +0700

    Cteate a GetFloatRandBetween method

--- randomizer.go
+++ randomizer.go
@@ -2,11 +2,18 @@ package gorand
 
 import (
     "time"
+    "math"
     "math/rand"
 )
 
 // Returns a random number between min and max numbers
-func GetRandBetween(min, max int) int {
+func GetIntRandBetween(min, max int) int {
     rand.Seed(time.Now().UnixNano())
     return rand.Intn(max - min) + min
+}
+
+func GetFloatRandBetween(min, max float64) float64 {
+	rand.Seed(time.Now().UnixNano())
+	v := min + rand.Float64() * (max - min)
+	return math.Round(v * 100) / 100
 }
